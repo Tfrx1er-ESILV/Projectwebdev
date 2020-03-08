d9fd91bf9e8bae55e7e6562dbc1b1a9c2072fce6
Tfrx1er <theophile.freixa@outlook.fr>
2020-03-08 17:59:47 +0100

    Better version

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import { strict } from "assert";
 
 console.log("Hello World!");
+
+
+
 document.querySelector("#submit")
     .addEventListener("click",function writeToChat(event){
         var message = document.querySelector("#msg").value
+        var pseudo = document.querySelector("#nickname").value
         var discussionElt = document.querySelector(".discussion")
-        discussionElt.innerHTML += "<p>"+message  
+        var today = new Date();
+        var time = today.getHours() + ":" + today.getMinutes();
+        discussionElt.innerHTML += '<p style="color:white;">'+"<i>"+"at "+time+" | "+pseudo+" said :  "+"</i>"+message  
     })
