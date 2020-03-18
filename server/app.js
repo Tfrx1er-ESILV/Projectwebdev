45ed57545379ccda8288381ccca790235d52ba36
Théophile Freixa <theophilefreixa@MacBook-Pro-de-Theophile.local>
2020-03-18 15:16:05 +0100

    All by my sellllffff don't want be alllll byyyy my sellllllfffff anymoreeee

--- server/app.js
+++ server/app.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path')
 
 //app.use('*/css',express.static('./main.css'));
 //app.use('*/js',express.static('./index.js'));
-app.use(express.static('dist'));
+app.use(express.static(path.resolve(__dirname,'../client/dist')));
 
 app.use(function (req, res, next) {
-    res.status(404).sendfile('./dist/404.html');
+    res.status(404).sendFile(path.resolve(__dirname,'../client/dist/404.html'));
   })
   
 app.listen(port, () => console.log(`Now go to http://localhost:${port}!`))
